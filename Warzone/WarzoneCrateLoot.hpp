b82e2957f8f76d3248b85637ea5c2e03f23118fa
Pierce <123277032+0Pierce@users.noreply.github.com>
2023-09-29 12:53:12 -0400

    Fixes

---
+++ Warzone/WarzoneCrateLoot.hpp
@@ -0,0 +1,126 @@
+class WarzoneCrateLoot{
+
+
+class Common{
+
+ weapons[] = {
+            {"hgun_Pistol_Signal_F",2},
+            {"hgun_PDW2000_F",3},
+            {"SMG_01_F",6},
+            {"SMG_03C_TR_hex",2},
+            {"SMG_03_TR_khaki",1},
+            {"SMG_03_camo",3},
+            {"arifle_TRG21_F",1},
+            {"arifle_Katiba_F",4},
+            {"arifle_CTARS_hex_F",1},
+            {"sgun_HunterShotgun_01_sawedoff_F",2},
+            {"srifle_DMR_06_camo_F",2},
+            {"srifle_DMR_04_Tan_F",1}
+        };
+
+
+	items[] = {
+            {"optic_AMS_snd",2},
+            {"optic_LRPS",1},
+            {"muzzle_snds_B_khk_F",3},
+            {"bipod_02_F_lush",1}
+        };
+
+
+};
+
+class Rare{
+
+ weapons[] = {
+            {"hgun_Pistol_01_F",4},
+            {"arifle_AKS_F",1},
+            {"arifle_MXC_khk_F",2},
+            {"arifle_ARX_ghex_F",4},
+            {"LMG_Mk200_F",1},
+            {"sgun_HunterShotgun_01_sawedoff_F",1},
+            {"sgun_HunterShotgun_01_F",3},
+            {"arifle_SPAR_03_blk_F",1},
+            {"srifle_EBR_F",2},
+            {"srifle_LRR_F",1}
+        };
+        magazines[] = {
+            {"20Rnd_762x51_Mag",17},
+            {"2Rnd_12Gauge_Pellets",8},
+            {"6Rnd_12Gauge_Slug",16},
+            {"10Rnd_127x54_Mag",2},
+            {"UGL_FlareYellow_F",1}
+        };
+        items[] = {
+            {"muzzle_snds_B",2}
+        };
+
+
+
+};
+
+
+
+class Epic{
+  weapons[] = {
+            {"arifle_Mk20C_F",2},
+            {"SMG_03C_khaki",3},
+            {"SMG_02_F",6},
+            {"arifle_MXC_Black_F",1},
+            {"arifle_MXM_khk_F",3},
+            {"arifle_MSBS65_Mark_camo_F",2},
+            {"arifle_AKM_F",7},
+            {"arifle_AK12U_F",2},
+            {"LMG_03_F",1},
+            {"sgun_HunterShotgun_01_F",1},
+            {"srifle_DMR_01_F",3},
+            {"LMG_Zafir_F",1},
+            {"srifle_DMR_02_camo_F",1}
+        };
+
+
+		 items[] = {
+            {"muzzle_snds_93mmg_tan",1},
+            {"optic_Hamr",2},
+            {"bipod_02_F_tan",2}
+        };
+
+
+};
+
+
+class Legendary{
+
+	 weapons[] = {
+            {"arifle_MXM_khk_F",1},
+            {"arifle_MSBS65_black_F",3},
+            {"arifle_MSBS65_UBS_camo_F",6},
+            {"arifle_AKM_F",1},
+            {"arifle_AK12U_F",3},
+            {"LMG_Mk200_F",2},
+            {"arifle_RPK12_lush_F",1},
+            {"arifle_MX_SW_khk_F",1},
+            {"SMG_03_TR_camo",1},
+            {"sgun_HunterShotgun_01_F",1},
+            {"srifle_DMR_01_F",1},
+            {"srifle_DMR_05_tan_f",1)}
+        };
+        magazines[] = {};
+        items[] = {
+            {"optic_LRPS",2},
+            {"optic_Arco_ghex_F",1},
+            {"muzzle_snds_B",2},
+            {"muzzle_snds_L",3},
+            {"optic_Aco",4},
+            {"bipod_01_F_blk",3},
+            {"muzzle_snds_B_lush_F",1},
+            {"muzzle_snds_65_TI_blk_F",1},
+            {"muzzle_snds_65_TI_hex_F",2}
+        };
+
+}
+
+
+
+
+
+}
